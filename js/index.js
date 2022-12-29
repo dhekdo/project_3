4f414f3bb58f8b0ef7dc78c460c403ceae333275
dhekdo <daae0924@naver.com>
2022-12-29 16:14:52 +0900

    2022.12.29

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-
+// 줄 잡아당기면 홈화면 전환
 $("#mainSwi").click(function(){
-    $(".mainBg_off").addClass("bgOff");
+    $(".mainBg_off").fadeOut(2000);
     $(".mainBg_on").show();
     $("#mainSwi").css("cursor", "default");
 });
